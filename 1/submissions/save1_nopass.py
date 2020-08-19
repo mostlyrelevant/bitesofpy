9e1c14d3540149bd7050dc31a334698f9f7ad625
MR <jdtolzmann@gmail.com>
2020-08-19 08:15:50 -0600

    submission Bite 1 @ codechalleng.es

---
+++ 1/submissions/save1_nopass.py
@@ -0,0 +1,7 @@
+def sum_numbers(numbers=None):
+    pass
+
+    if numbers is None:
+        numbers = range(1,100)
+
+    return sum(numbers)
---
+++ 1/summing.py
@@ -0,0 +1,8 @@
+def sum_numbers(numbers=None):
+    pass
+
+    # my code
+    if numbers is None:
+        numbers = range(1,100)
+
+    return sum(numbers)
