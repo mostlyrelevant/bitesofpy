9678c8ee5cbf940465e8e277d9c5630ea607dc16
MR <jdtolzmann@gmail.com>
2020-08-20 08:11:00 -0600

    Final save for bite 1

--- 1/summing.py
+++ 1/summing.py
@@ -1,6 +1,14 @@
 def sum_numbers(numbers=None):
     pass
 
+    """ solution
+
+    if numbers is None:
+        numbers = range(1, 101)
+    return sum(numbers)
+
+    """
+
     # my code
     if numbers is None:
         numbers = range(101)
